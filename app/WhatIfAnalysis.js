3aff3241d44773f3fc0b7c3d0939d36ddfeb0985
RobinSardja <robinsardja@gmail.com>
2024-11-18 15:23:24 -0500

    Added function to calculate future GPA.

--- app/WhatIfAnalysis.js
+++ app/WhatIfAnalysis.js
@@ -1,12 +1,34 @@
-function FutureGPA() {
+function calcFutureGPA( currGrades, currCredits, futureGrades, futureCredits ) {
+    const letterGradeToGPA = new Map([
+        ['A', 4], ['B', 3], ['C', 2], ['D', 1], ['F', 0]
+    ])
+
+    let n1 = currGrades.length, n2 = futureGrades.length
+    let currCreditSum = 0, futureCreditSum = 0
+    let gradeSum = 0
+
+    for( let i = 0; i < n1; i++ ) {
+        gradeSum += letterGradeToGPA.get( currGrades[i] ) * currCredits[i];
+        currCreditSum += currCredits[i];
+    }
+    for( let i = 0; i < n2; i++ ) {
+        gradeSum += letterGradeToGPA.get( futureGrades[i] ) * futureCredits[i];
+        futureCreditSum += futureCredits[i];
+    }
+
+    return gradeSum / ( currCreditSum + futureCreditSum )
+}
+
+function FutureGPAComponent() {
     return (
         <section>
             <h1>Future GPA</h1>
+            <p>{ calcFutureGPA( ['A', 'A'], [3, 3], ['A', 'A'], [3, 3] ).toFixed(2) }</p>
         </section>
     )
 }
 
-function DesiredGPA() {
+function DesiredGPAComponent() {
     return (
         <section>
             <h1>Desired GPA</h1>
@@ -18,8 +40,8 @@ export default function WhatIfAnalysis() {
     return (
         <section>
             <h1>What-If Analysis</h1>
-            <FutureGPA />
-            <DesiredGPA />
+            <FutureGPAComponent />
+            <DesiredGPAComponent />
         </section>
     );
 }
