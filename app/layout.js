dae8ffbf193ed39044cb65c638eded07c60cc8e3
Mohammad Aatif Shaikh <78430955+aatif101@users.noreply.github.com>
2024-12-05 02:50:21 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,18 @@
-import "./globals.css";
+"use client"
 
-export const metadata = {
-  title: "CourseWorks",
-  description: "USF Fall 2024 Group 29 Software Engineering Topic 2 Final Project",
-};
+import "./globals.css";
+import { useState, createContext } from "react";
+
+export const AuthContext = createContext(null);
 
 export default function RootLayout({ children }) {
+  const [user, setUser] = useState(null);
+
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <AuthContext.Provider value={{ user, setUser }}>
+        <body>{children}</body>
+      </AuthContext.Provider>
     </html>
   );
 }
